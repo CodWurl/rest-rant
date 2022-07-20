56fdda9ec81ac329d61719872367a365385a3a8b
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-20 18:58:16 -0400

    Added image to places page

--- controllers/places.js
+++ controllers/places.js
@@ -1,17 +1,26 @@
 // Invoked for any requests passed to this router
 const router = require('express').Router()
 
+
 // Handles places request
 router.get('/', (req, res)=>{
-    res.send('GET/ places')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index'),{places}
 
 })
-// GET /places
-app.get('/', (req, res) => {
-    let places = []
-    res.render('places/index', {places})
-
-  })
 
 //Separating app into multiple modules
 //code to export from index.js 
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,7 +1,19 @@
-// GET /places
-app.get('/', (req, res) => {
-    let places = []
-    res.render('places/index', {places})
-
-  })
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
+    })
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+  )
+  }
   
