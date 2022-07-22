111e391d0a49a5dd9cb92a3dd580b5608ba7c92e
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-22 17:31:38 -0400

    testing centering home text

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,9 @@ function home() {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+             
+            <h1>HOME</h1>
+            <link rel="stylesheet" href="/css/style.css" />
             </main>
             <a href="/places"><button className="btn-primary">Places Page</button></a>
         </Def>
