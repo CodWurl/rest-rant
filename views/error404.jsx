1f5327ae6142cb8e445ad845cd375fe58440b88a
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-23 00:15:09 -0400

    Added error page image

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,7 +7,11 @@ return (
     <Def>
         <main>
             <h1>404 : PAGE NOT FOUND</h1>
+            <div>
+            <img src ="/images/error-404.gif" alt="error404" />
+            </div>
             <p>Oops, sorry, we can't find this page!</p>
+          
         </main>
     </Def>
 )
